b91376d41cf7f442a691055a2bf52aa59e67ceba
lorkorblaq <f3miolok@gmail.com>
2024-04-14 06:54:07 +0100

    update

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from website import create_app, socketio
 
 app = create_app()
 if __name__ == "__main__":
-    socketio.run(app, host="13.53.70.208", port=8080, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8080, debug=True)
